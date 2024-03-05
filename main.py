1cffaa44b1bca58df860546603cd407260e2ae19
Aadhil M. Syed <144854365+aadhilmsyed@users.noreply.github.com>
2024-03-05 14:23:31 -0800

    Add Seed Argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 from board import TicTacToe
 from players import Human, SimpleAI, RandomAI, MonteCarloAI, MinimaxAI, AlphaBetaAI
 
@@ -7,11 +8,13 @@ parser.add_argument('-w', default=3, type=int, help='Rows of game')
 parser.add_argument('-l', default=3, type=int, help='Columns of game')
 parser.add_argument('-p1', default='human', type=str, help='Player 1 agent. Use any of the following: [human, stupidAI, randomAI, monteCarloAI, minimaxAI, alphaBetaAI]')
 parser.add_argument('-p2', default='human', type=str, help='Player 2 agent. Use any of the following: [human, stupidAI, randomAI, monteCarloAI, minimaxAI, alphaBetaAI]')
+parser.add_argument('-seed', default=0, type=int, help='Seed for Randomization. Enter an Integer Value.')
 
 args = parser.parse_args()
 
 w = args.w
 l = args.l
+random.seed(args.seed)
 
 agents = {'human': Human, 'simpleAI': SimpleAI, 'randomAI': RandomAI, 'monteCarloAI': MonteCarloAI, 'minimaxAI': MinimaxAI, 'alphaBetaAI': AlphaBetaAI}
 
